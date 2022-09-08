28883cb36b5bbdc8daabec3d80549b80fedf7eef
Ambar <Ambarpg80@gmail.com>
2022-09-08 13:41:42 -0400

    commit tree 2a8f21c9773a9de6fa8a9865286a84d08cfe0c72

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Petra",
+    streetAddress: '1234 Sesame St' ,
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = {...employee};
+    employees[key] = value;
+    return employees;
+};
+updateEmployeeWithKeyAndValue()
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const deletedEmployees = {...employee};
+    delete deletedEmployees[key];
+    return deletedEmployees;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key] ;
+    return employee;
+}
+/**/
